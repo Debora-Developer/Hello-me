c3d975d22a0ebc4daa1c7f3fdd8be21ddab3a4aa
worgho2 <otavio.baziewicz.filho@gmail.com>
2025-04-19 14:48:12 -0300

    feat: Add popping images

--- Cueio.swiftpm/ResultView.swift
+++ Cueio.swiftpm/ResultView.swift
@@ -1,26 +1,60 @@
 import SwiftUI
+import SpriteKit
 
 struct ResultView: View {
     @Environment(QuizDatabase.self) private var data
     @Binding var start : Bool
     
+    // var body: some View {
+    //     VStack {
+    //         Spacer()
+    //         Text("Parabéns")
+    //             .font(.title2)
+    //         Spacer()
+    //         Button {
+    //             withAnimation(.bouncy) {
+    //                 data.restartQuiz()
+    //                 start = false
+    //             }
+    //         } label: {
+    //             Text("Refazer Quiz")
+    //                 .foregroundStyle(.blue)
+    //         }
+    //         .buttonStyle(.bordered)
+    //         Spacer()
+    //     }
+    // }
+
+    var scene: SKScene {
+        let s = PhysicsScene(size: CGSize(width: 800, height: 800))
+        s.scaleMode = .aspectFit
+        return s
+    }
+
     var body: some View {
-        VStack {
-            Spacer()
-            Text("Parabéns")
-                .font(.title2)
-            Spacer()
-            Button {
-                withAnimation(.bouncy) {
-                    data.restartQuiz()
-                    start = false
+        ZStack {
+            VStack {
+                Spacer()
+                Text("Parabéns")
+                    .font(.title2)
+                Spacer()
+                Button {
+                    withAnimation(.bouncy) {
+                        data.restartQuiz()
+                        start = false
+                    }
+                } label: {
+                    Text("Refazer Quiz")
+                        .foregroundStyle(.blue)
                 }
-            } label: {
-                Text("Refazer Quiz")
-                    .foregroundStyle(.blue)
+                .buttonStyle(.bordered)
+                Spacer()
             }
-            .buttonStyle(.bordered)
-            Spacer()
+            .zIndex(1)
+
+            SpriteView(scene: scene)
+                .ignoresSafeArea()
+                .zIndex(0)
         }
     }
 }
